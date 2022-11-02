fd1da4fdf9ac5fdedc20e68d3a49655e9b3c4337
Jérémie Dumas <jeremie.dumas@ens-lyon.org>
2022-11-01 18:05:11 -0700

    Update github action.

--- tests/test_foo.cpp
+++ tests/test_foo.cpp
@@ -25,6 +25,20 @@ TEST_CASE("parallel_cow_tbb2", "[tbb]")
     REQUIRE(x == 4950);
 }
 
+TEST_CASE("parallel_cow_tbb3", "[tbb]")
+{
+    std::atomic_int x = 0;
+    tbb::parallel_for(0, 100, [&](int i) { x += i; });
+    REQUIRE(x == 4950);
+}
+
+TEST_CASE("parallel_cow_tbb4", "[tbb]")
+{
+    std::atomic_int x = 0;
+    tbb::parallel_for(0, 100, [&](int i) { x += i; });
+    REQUIRE(x == 4950);
+}
+
 TEST_CASE("parallel_cow_std", "[std]")
 {
     std::atomic_int x = 0;
@@ -33,6 +47,20 @@ TEST_CASE("parallel_cow_std", "[std]")
 }
 
 TEST_CASE("parallel_cow_std2", "[std]")
+{
+    std::atomic_int x = 0;
+    foo::ThreadPool::parallel_for(0, 100, [&](int i) { x += i; });
+    REQUIRE(x == 4950);
+}
+
+TEST_CASE("parallel_cow_std3", "[std]")
+{
+    std::atomic_int x = 0;
+    foo::ThreadPool::parallel_for(0, 100, [&](int i) { x += i; });
+    REQUIRE(x == 4950);
+}
+
+TEST_CASE("parallel_cow_std4", "[std]")
 {
     std::atomic_int x = 0;
     foo::ThreadPool::parallel_for(0, 100, [&](int i) { x += i; });
